2d9b16cd5543fef8c9f0de84f03f61c4b01d89ef
Julien Robert <julien.robert.info@gmail.com>
2025-01-18 10:45:22 +0100

    fix: disable email credentials

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -10,10 +10,25 @@ import {
 	getServerSession,
 } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
-import EmailProvider from 'next-auth/providers/email'
 import prisma from '@/utils/prisma'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { PrismaClient } from '@prisma/client'
+
+export const transportOptions = {
+	name:
+		process.env.NODE_ENV === 'production'
+			? process.env.EMAIL_NAME
+			: process.env.EMAIL_SERVER,
+	host: process.env.EMAIL_SERVER,
+	port: Number(process.env.EMAIL_PORT),
+	secure: false,
+	...(process.env.NODE_ENV === 'production' && {
+		auth: {
+			user: process.env.EMAIL_USER,
+			pass: process.env.EMAIL_PWD,
+		},
+	}),
+}
 
 export const authConfig = {
 	providers: [
@@ -29,12 +44,16 @@ export const authConfig = {
 				}
 			},
 		}),
-		EmailProvider({
-			server: process.env.EMAIL_SERVER,
-			from: process.env.EMAIL_FROM,
-		}),
+		// EmailProvider({
+		// 	server: transportOptions,
+		// 	from: process.env.EMAIL_FROM,
+		// }),
 	],
 	adapter: PrismaAdapter(prisma as unknown as PrismaClient),
+	theme: {
+		colorScheme: 'light',
+		brandColor: '#99c1de',
+	},
 } satisfies NextAuthOptions
 
 export const auth = async (
--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -1,26 +1,16 @@
 'use server'
 import { createTransport } from 'nodemailer'
+import { transportOptions } from './auth'
 
 export const inviteMail = async (email: string, groupName: string) => {
-	const transport = createTransport({
-		host: process.env.EMAIL_SERVER,
-		port: Number(process.env.EMAIL_PORT),
-		secure: false,
-		...(process.env.NODE_ENV === 'production' && {
-			auth: {
-				user: process.env.EMAIL_USER,
-				pass: process.env.EMAIL_PWD,
-			},
-		}),
-	})
+	const transport = createTransport(transportOptions)
 
 	const result = await transport.sendMail({
 		from: process.env.EMAIL_FROM,
 		to: email,
-		subject: `You have been invited to join ${groupName}`,
-		text: `You have been invited to join ${groupName}. Copy and paste the following link into your browser: ${process.env.NEXTAUTH_URL}`,
+		subject: `Invitation à rejoindre Fair spend`,
+		html: mailContent(groupName),
 	})
-	console.log('inviteMail', result)
 
 	const failed = result.rejected.concat(result.pending).filter(Boolean)
 
@@ -28,3 +18,134 @@ export const inviteMail = async (email: string, groupName: string) => {
 		throw new Error(`Email (${failed.join(', ')}) could not be sent`)
 	}
 }
+
+const mailContent = (groupName: string) => `<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Invitation à rejoindre FairSpend</title>
+    <style>
+        @media only screen and (max-width: 620px) {
+            table[class=body] h1 {
+                font-size: 28px !important;
+                margin-bottom: 10px !important;
+            }
+            table[class=body] p,
+            table[class=body] ul,
+            table[class=body] ol,
+            table[class=body] td,
+            table[class=body] span,
+            table[class=body] a {
+                font-size: 16px !important;
+            }
+            table[class=body] .wrapper,
+            table[class=body] .article {
+                padding: 10px !important;
+            }
+            table[class=body] .content {
+                padding: 0 !important;
+            }
+            table[class=body] .container {
+                padding: 0 !important;
+                width: 100% !important;
+            }
+            table[class=body] .main {
+                border-left-width: 0 !important;
+                border-radius: 0 !important;
+                border-right-width: 0 !important;
+            }
+            table[class=body] .btn table {
+                width: 100% !important;
+            }
+            table[class=body] .btn a {
+                width: 100% !important;
+				text-decoration: "none";
+            }
+            table[class=body] .app-name {
+                font-weight: 700;
+            }
+        }
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            word-spacing: normal;
+            background-color: #f5f5f5;
+			color: rgba(0, 0, 0, 0.8);
+        }
+    </style>
+</head>
+<body>
+    <table border="0" cellpadding="0" cellspacing="0" class="body">
+        <tr>
+            <td>&nbsp;</td>
+            <td class="container">
+                <div class="content">
+                    <table class="main">
+                        <tr>
+                            <td class="wrapper">
+                                <table border="0" cellpadding="0" cellspacing="0">
+                                    <tr>
+                                        <td>
+                                            <h1 class="app-name" style="max-width: 200px;">
+												Fair spend
+											</h1>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                    <table class="main" bgcolor="#ffffff">
+                        <tr>
+                            <td class="wrapper">
+                                <table border="0" cellpadding="0" cellspacing="0">
+                                    <tr>
+                                        <td>
+                                            <h1>Rejoignez le groupe de dépenses de ${groupName}</h1>
+                                            <p>[Nom de l'expéditeur] vous invite à rejoindre le groupe de partage de dépenses sur FairSpend.</p>
+                                            <table border="0" cellpadding="0" cellspacing="0" class="btn btn-primary">
+                                                <tbody>
+                                                    <tr>
+                                                        <td align="left">
+                                                            <table border="0" cellpadding="0" cellspacing="0">
+                                                                <tbody>
+                                                                    <tr>
+                                                                        <td>
+																			<a href="${process.env.NEXTAUTH_URL}" target="_blank">Rejoindre le groupe</a>
+																		</td>
+                                                                    </tr>
+                                                                </tbody>
+                                                            </table>
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                    <table class="main" bgcolor="#ffffff">
+                        <tr>
+                            <td class="wrapper">
+                                <table border="0" cellpadding="0" cellspacing="0">
+                                    <tr>
+                                        <td>
+                                            <h2>Qu'est-ce que FairSpend ?</h2>
+                                            <p>FairSpend est une application qui vous aide à gérer facilement le partage des dépenses avec vos colocataires ou amis. Grâce à différents modes de répartition des coûts, un suivi simplifié des dettes et un compte commun optionnel, FairSpend rend la gestion budgétaire équitable et transparente pour tous.</p>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </td>
+            <td>&nbsp;</td>
+        </tr>
+    </table>
+</body>
+</html>`
